72bf9a26cfff9f0864ce146e061f60b06c902fdd
Declan Whelan <dwhelan@dpwhelan.com>
2017-12-12 17:24:01 -0500

    Convert bar tender to ECMA 6
    Used let, const and arrow functions.

--- bartender/bartender.spec.js
+++ bartender/bartender.spec.js
@@ -1,13 +1,13 @@
 ﻿'use strict';
 
-var Bartender = require('./bartender');
+const Bartender = require('./bartender');
 require('should');
 
-xdescribe('Making a drink', function () {
-  describe('Charging for drinks', function () {
-    it('Should charge $9.99 for a mockito', function () {
-      var bartender = new Bartender();
-      var cost = bartender.makeADrink('mockito');
+xdescribe('Making a drink', () =>{
+  describe('Charging for drinks', () =>{
+    it('Should charge $9.99 for a mockito', () =>{
+      const bartender = new Bartender();
+      const cost = bartender.makeADrink('mockito');
       cost.should.equal(9.99);
     });
   });
